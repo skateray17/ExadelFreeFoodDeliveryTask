f5a31c240a8f585a027d9ec549b657c89bcf5e63
Anna <fpm.pankova@bsu.by>
2018-04-13 21:48:42 +0300

    imports and exports working

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-import handlebars from 'handlebars';
+import { square } from './loginContent.js';
 
+const h1 = document.createElement('h1');
+h1.innerText = square(5);
+document.querySelector('body').appendChild(h1);
