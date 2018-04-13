d54cad37b4c1e679abe9c279d75a64477a915571
Anna <fpm.pankova@bsu.by>
2018-04-13 08:54:24 +0300

    babel added

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,18 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 };
