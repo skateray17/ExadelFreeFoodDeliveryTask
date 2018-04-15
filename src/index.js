77265ab0f742cec78c736472c97168eb3ec1b103
Anna <fpm.pankova@bsu.by>
2018-04-15 11:00:44 +0300

    works

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import { square } from './screens/loginContent/loginContent.js';
+import handlebars from 'handlebars';
+import { loginScreen } from './screens/loginContent/loginContent';
+let html = handlebars.compile(loginScreen());
 
-const h1 = document.createElement('h1');
-h1.innerText = square(5);
-document.querySelector('body').appendChild(h1);
+const screen = document.getElementById('screen').innerHTML;
+
+const context = { loginScreen: handlebars.compile(loginScreen()) };
+document.getElementById('main-screen').innerHTML = html(context);
+
+console.log('html: ' + html);
+console.log('context: ' + context);
+console.log('document.getElementById(\'main-screen\').innerHTML: ' + document.getElementById('main-screen').innerHTML);
--- src/screens/loginContent/loginContent.js
+++ src/screens/loginContent/loginContent.js
@@ -1,8 +1,8 @@
 import loginContent from './loginContent.html';
 
-export function square(x) {
-  return x * x;
+export function loginScreen() {
+    return loginContent;
 }
 
-console.log(loginContent);
-
+console.log('loginContent: ' + loginContent);
+console.log('loginContent.innerHTML: ' + loginContent.innerHTML);
