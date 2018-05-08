b9fc802752d05732fcb532ff1c673e6453125b4d
Anna <fpm.pankova@bsu.by>
2018-05-08 08:51:08 +0300

    header and color

--- src/components/userContent/cardTemplate/card.js
+++ src/components/userContent/cardTemplate/card.js
@@ -25,6 +25,8 @@ export default class Card {
     target.innerHTML = '';
     const cardItem = target.appendChild(createElementsFromString(cardTemplate));
     this.insertCardContent(cardItem, props, target);
+
+    return cardItem;
   }
 
   createOrderItem(cardItem, order) {
