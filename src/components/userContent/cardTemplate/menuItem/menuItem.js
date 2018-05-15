03cd11a5e7c83ca801cccf293ef2ece3c5c1b56b
Egor Karabanovich <karabanovich333@gmail.com>
2018-05-15 13:53:55 +0300

    fix menuItem listener

--- src/components/userContent/cardTemplate/menuItem/menuItem.js
+++ src/components/userContent/cardTemplate/menuItem/menuItem.js
@@ -8,33 +8,35 @@ export default class MenuItem {
       quantity: 0,
       index: 0,
     };
+    this.contentListener = this.contentListener.bind(this);
   }
   updateHTML(el) {
     const quantity = el.querySelector('.quantity');
     quantity.innerHTML = this.state.quantity;
   }
+  contentListener(event) {
+    if (event.target.className.includes('incr') || event.target.className.includes('decr')) {
+      let C = true;
+      if (event.target.className.includes('decr')) {
+        C = false;
+      }
+      if (C) {
+        this.state.quantity++;
+      } else if (this.state.quantity !== 0) {
+        this.state.quantity--;
+      }
+      this.updateHTML(event.currentTarget);
+    }
+    this.callback(this.state);
+  }
   render(target, props) {
     this.state.quantity = props.quantity;
     this.state.index = props.index;
+    this.callback = props.callback;
     const menuItemTemplate = menuItem(props);
     const elemDOM = createElementsFromString(menuItemTemplate);
     target.appendChild(elemDOM);
-    elemDOM.addEventListener('click', (event) => {
-      const editContent = elemDOM;
-      if (event.target.className.includes('incr') || event.target.className.includes('decr')) {
-        let C = true;
-        if (event.target.className.includes('decr')) {
-          C = false;
-        }
-        if (C) {
-          this.state.quantity++;
-        } else if (this.state.quantity !== 0) {
-          this.state.quantity--;
-        }
-        this.updateHTML(editContent);
-      }
-      props.callback(this.state);
-    });
+    elemDOM.addEventListener('click', this.contentListener);
     return elemDOM;
   }
 }
