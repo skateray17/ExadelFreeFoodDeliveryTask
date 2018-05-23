32c01cc9b9ea1281744bfc4a6d5ddcfa300f5502
Anna <fpm.pankova@bsu.by>
2018-05-23 17:24:30 +0300

    comment deleted

--- src/index.js
+++ src/index.js
@@ -2,20 +2,5 @@ import './index.css';
 import Router from './router';
 import { routes } from './config';
 
-/*
-usersScreen.render(target, props);
-usersScreen.update({
-  id: 1525986000000,
-  header: {
-    active: 'true',
-    date: 'Fri May 11 2018',
-    weekday: 'Пт',
-  },
-  orders: [
-  ],
-  orderPrice: 6.4,
-});
-
-*/
 console.log(process.env.DB_HOST);
 const router = new Router(routes, document.getElementById('screen'));
