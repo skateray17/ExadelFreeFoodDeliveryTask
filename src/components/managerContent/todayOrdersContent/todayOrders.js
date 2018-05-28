47c8884f79828fda2bcf49fecb4494f354a7f73a
Maxim Bykov <maxim12098@gmail.com>
2018-05-28 16:37:19 +0300

    print button added

--- src/components/managerContent/todayOrdersContent/todayOrders.js
+++ src/components/managerContent/todayOrdersContent/todayOrders.js
@@ -13,14 +13,27 @@ export default class {
     this.getProps()
       .then((res) => {
         (new OrdersInner()).render(elem, res);
-      })
-      .finally(() => spinner.destroy());
+        document.querySelector('.today-orders_print-button').addEventListener('click', () => {
+          const printContents = document.querySelector('.today-orders__container').outerHTML;
+          const screen = document.getElementById('screen');
+          screen.style.display = 'none';
+          const newLi = document.createElement('div');
+          newLi.innerHTML = printContents;
+          document.body.insertBefore(newLi, null);
+          newLi.querySelector('.today-orders_print-button').style.display = 'none';
+          window.print();
+          document.body.removeChild(newLi);
+          screen.style.display = '';
+        });
+      }).finally(() => {
+        spinner.destroy();
+      });
     target.appendChild(elem);
     return elem;
   }
 
   getProps() {
-    return get('order/', { }, { currentDate: new Date() })
+    return get('order/', {}, { currentDate: new Date() })
       .then((res) => {
         if (res.ok) {
           return res.json();
