61e5eb2c973c30cee0ccf1df0a39b48299f4c139
Pavel Ramasheuski <pavel.ramasheuski@gmail.com>
2018-07-31 14:18:43 +0300

    error page now appear more often

--- src/router.js
+++ src/router.js
@@ -53,7 +53,10 @@ export default class Router {
         this.navigate(this.routes.rootPath);
         return;
       }
-
+      if (url.split('/').length > 4) {
+        this.navigate('error');
+        return;
+      }
       const props = getUrlParams();
 
       this.cleanContainer();
@@ -63,6 +66,12 @@ export default class Router {
         this.navigate('error');
       }
       const field = temp.substring(1);
+      console.log(url);
+      console.log(hash);
+      console.log(temp);
+      console.log(field);
+      console.log(this.routs);
+
       if (this.routes.hasOwnProperty(field)) {
         this.checkGuards(this.routes[field].guards, field).then((isAllowed) => {
           if (isAllowed) {
